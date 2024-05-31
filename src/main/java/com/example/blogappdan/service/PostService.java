5ae2ca9d5033582ee798c8496f52d0701ac04688
mariaprang <mprangishvili@newwork.com>
2024-05-31 22:29:44 +0400

    created new branch, replaced autowired to constructor injection

--- src/main/java/com/example/blogappdan/service/PostService.java
+++ src/main/java/com/example/blogappdan/service/PostService.java
@@ -2,14 +2,15 @@ package com.example.blogappdan.service;
 
 import com.example.blogappdan.entity.Post;
 import com.example.blogappdan.repository.PostRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class PostService {
 
-    @Autowired
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public Post createPost(String title, String text){
         return postRepository.save(new Post(title, text));
